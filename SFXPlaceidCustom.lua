3a452138d60c9f307d33a10cea9e9bdd375909eb
Sortex [ SFX ] <130068806+LorisHost@users.noreply.github.com>
2025-03-22 20:33:15 +0200

    Decrypting Place Ids
    Now you can't getting Dectecting

--- SFXPlaceidCustom.lua
+++ SFXPlaceidCustom.lua
@@ -1,1 +1,13 @@
+local ePlaces = {
+    ["X5PGA=="] = {id = 112233445, script = "https://your-url.com/pressure.lua"},
+    ["X9JBE=="] = {id = 987654321, script = "https://your-url.com/evade.lua"},
+    ["Y3KHC=="] = {id = 123456789, script = "https://your-url.com/forsaken.lua"}
+}
 
+for e, data in pairs(encodedPlaces) do
+    if game.PlaceId == data.id then
+        print("🔹 Detected:", encoded)
+        loadstring(game:HttpGet(data.script))()
+        break
+    end
+end
