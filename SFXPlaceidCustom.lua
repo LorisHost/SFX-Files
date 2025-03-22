dac85e8be621b2c474612e0961c08e9ecd3789c2
Sortex [ SFX ] <130068806+LorisHost@users.noreply.github.com>
2025-03-22 20:37:11 +0200

    Updated The File Place Id

--- SFXPlaceidCustom.lua
+++ SFXPlaceidCustom.lua
@@ -1,12 +1,12 @@
-local ePlaces = {
+local e = {
     ["X5PGA=="] = {id = 112233445, script = "https://your-url.com/pressure.lua"},
     ["X9JBE=="] = {id = 987654321, script = "https://your-url.com/evade.lua"},
     ["Y3KHC=="] = {id = 123456789, script = "https://your-url.com/forsaken.lua"}
 }
 
-for e, data in pairs(encodedPlaces) do
+for key, data in pairs(e) do
     if game.PlaceId == data.id then
-        print("🔹 Detected:", encoded)
+        print("Detected:", key)
         loadstring(game:HttpGet(data.script))()
         break
     end
