a8e520127d2cde6a961cae02724c1fee3153a165
Sortex [ SFX ] <130068806+LorisHost@users.noreply.github.com>
2025-03-23 10:57:27 +0200

    Sigma Sigma Boy 🤨

--- SFXPrivateScripts.lua
+++ SFXPrivateScripts.lua
@@ -1,2 +1,4 @@
 -- Hiding Function getgc
 hookfunction(getgc, function(...) return checkcaller() and getgc(...) or {} end)
+-- Hiding Function FireServer()
+hookfunction(game.ReplicatedStorage.RemoteEvent.FireServer, function(...) return checkcaller() and hookfunction(game.ReplicatedStorage.RemoteEvent.FireServer, ... ) or nil end)
