796baf26c59d025d786e87c1934b07900b1041a2
Sortex [ SFX ] <130068806+LorisHost@users.noreply.github.com>
2025-03-23 11:08:10 +0200

    Update SFXPrivateScripts.lua
    Reversed.

--- SFXPrivateScripts.lua
+++ SFXPrivateScripts.lua
@@ -1,4 +1,13 @@
--- Hiding Function getgc
-hookfunction(getgc, function(...) return checkcaller() and getgc(...) or {} end)
--- Hiding Function FireServer()
-hookfunction(game.ReplicatedStorage.RemoteEvent.FireServer, function(...) return checkcaller() and hookfunction(game.ReplicatedStorage.RemoteEvent.FireServer, ... ) or nil end)
+local List = {
+    -- Hiding Function getgc
+    getgc = function() return hookfunction(getgc, function(...) return checkcaller() and getgc(...) or {} end) end,
+
+    -- Hiding Function FireServer()
+    FireServer = function() return hookfunction(game.ReplicatedStorage.RemoteEvent.FireServer, function(...)
+        return checkcaller() and game.ReplicatedStorage.RemoteEvent.FireServer(...) or nil
+    end) end
+}
+
+for _, bypass in pairs(List) do
+    bypass()
+end
